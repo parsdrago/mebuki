b4b4820ec4761e966514e8fe18e72f93d7237b8a
utan <iw.utan@gmail.com>
2022-03-09 00:14:56 +0900

    write core codes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const editorArea = document.getElementById('editor-area');
 const editor = document.getElementById('editor');
+const preview = document.getElementById('preview');
+const togglePreview = document.getElementById('toggle-preview');
+
+editor.onchange = function() {
+    document.location.hash = LZString.compressToEncodedURIComponent(editor.value);
+}
+
+togglePreview.onclick = function() {
+    editorArea.classList.toggle('preview');
+    preview.innerHTML = marked.parse(editor.value);
+}
+
+document.body.onload = function() {
+    if (document.location.hash) {
+        editor.value = LZString.decompressFromEncodedURIComponent(document.location.hash.substring(1));
+    }
+    togglePreview.onclick();
+}
